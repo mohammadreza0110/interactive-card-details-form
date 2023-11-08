1e14bf69cba6f7ec7d0f9081d2f527fe5d386fa2
Mhmdrza <mohammadreza0216@gmail.com>
2023-11-08 21:42:25 +0330

    backcard dynamic CVC

--- src/components/BackCard.jsx
+++ src/components/BackCard.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 
-export default function BackCard() {
+export default function BackCard({ CVC }) {
   return (
     <section className="absolute 2xl:static translate-x-[-50%] left-[50%] ml-4 bg-slate-300 xl:py-7 py-4 w-[300px] xl:w-[500px] rounded-lg 2xl:translate-x-64 xl:ml-auto 2xl:h-[280px]">
       <div className="w-full h-12 bg-black xl:h-20"></div>
       <div className="xl:w-[420px] px-6 m-auto">
         <div className="xl:p-3 p-2.5 text-right text-white rounded-md xl:mb-9 mt-5 xl:mt-7 bg-slate-400 mb-10">
-          000
+          {CVC}
         </div>
 
         <div className="flex-col hidden m-auto xl:flex xl:w-6/12 gap-y-2">
